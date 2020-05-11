3debf0c27e4ba3d81def6170f8b91a555a8aca82
fanzhengjie <fanzhengjie@youzan.com>
2020-05-11 10:22:35 +0800

    新增common模块的单测

--- progress-common/src/test/java/com/jary/progress/sensitice/SensitiveTest.java
+++ progress-common/src/test/java/com/jary/progress/sensitice/SensitiveTest.java
@@ -1,22 +1,29 @@
-package com.jary.progress.common.sensitive;
+package com.jary.progress.sensitice;
 
 import com.jary.progress.common.bean.UserDO;
 import com.jary.progress.common.util.SensitiveUtils;
+import org.junit.Before;
+import org.junit.Test;
 
 /**
  * @author fanzhengjie
- * @date 2020/5/9 3:43 下午
+ * @date 2020/5/11 9:56 上午
  */
-public class Main {
+public class SensitiveTest {
 
-    public static void main(String[] args) throws Exception {
-        UserDO userDO = new UserDO();
+    private UserDO userDO;
+
+    @Before
+    public void setUp(){
+        userDO = new UserDO();
         userDO.setId(1);
         userDO.setName("王三无");
         userDO.setAddress("浙江杭州");
         userDO.setSex((byte) 0);
+    }
 
+    @Test
+    public void testSensitive() throws Exception {
         System.out.println(SensitiveUtils.convert(userDO));
-
     }
 }
