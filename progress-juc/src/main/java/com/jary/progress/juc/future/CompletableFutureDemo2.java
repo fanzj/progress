5d590bd7f167f6e32a5ab7091d9ed9d8a27395ef
fanzhengjie <fanzhengjie@youzan.com>
2020-05-17 19:57:26 +0800

    异步计算

---
+++ progress-juc/src/main/java/com/jary/progress/juc/future/CompletableFutureDemo2.java
@@ -0,0 +1,43 @@
+package com.jary.progress.juc.future;
+
+import java.util.Random;
+import java.util.concurrent.*;
+
+/**
+ * @author fanzhengjie
+ * @date 2020/5/17 7:25 下午
+ */
+public class CompletableFutureDemo2 {
+
+    public static void main(String[] args) {
+        ExecutorService executorService = new ThreadPoolExecutor(4,4,0, TimeUnit.MILLISECONDS, new LinkedBlockingQueue<>());
+        CompletableFuture<String> completableFuture = CompletableFuture.supplyAsync(() -> {
+            try {
+                TimeUnit.MILLISECONDS.sleep(2000L);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            //仅仅为了测试异常类型
+            int i = 10 / 0;
+            return String.valueOf(new Random().nextInt(1000));
+        }, executorService);
+
+        try {
+            String result = completableFuture.get(2000L, TimeUnit.MILLISECONDS);
+            System.out.println("result = "+result);
+        } catch (ArithmeticException e) {
+            //异常类型被转换, 待研究
+            System.out.println("it is ArithmeticException error");
+        } catch (Exception e) {
+            //异常正常获取方法
+            Throwable throwable = e.getCause();
+            if(throwable instanceof ArithmeticException) {
+                System.out.println("it is ArithmeticException error ++");
+                throwable.printStackTrace();
+            }
+        }
+
+       // executorService.shutdown();
+    }
+}
---
+++ progress-juc/src/main/java/com/jary/progress/juc/future/FutureDemo2.java
@@ -0,0 +1,43 @@
+package com.jary.progress.juc.future;
+
+import java.text.MessageFormat;
+import java.util.concurrent.*;
+
+/**
+ * @author fanzhengjie
+ * @date 2020/5/17 7:48 下午
+ */
+public class FutureDemo2 {
+
+    public static void main(String[] args) {
+        long start = System.currentTimeMillis();
+        ExecutorService executorService = new ThreadPoolExecutor(5,5,0, TimeUnit.MILLISECONDS, new LinkedBlockingQueue<>());
+        Future<String> future = executorService.submit(() -> {
+            System.out.println("开始执行子线程的call方法");
+            try {
+                Thread.sleep(15000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            return "100";
+        });
+
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("main主线程开始执行任务");
+        try{
+            System.out.println("执行完子线程后返回的结果是：" + future.get());
+            long end = System.currentTimeMillis();
+            System.out.println(MessageFormat.format("耗时：{0}s", (end - start)/1000.0));
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
+        executorService.shutdown();
+    }
+}
