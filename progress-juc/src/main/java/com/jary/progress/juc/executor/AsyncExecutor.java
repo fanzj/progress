36bab9f85eb7c334eb36c4fee64563490ef77784
fanzhengjie <fanzhengjie@youzan.com>
2020-05-17 20:12:32 +0800

    添加线程池方法

---
+++ progress-juc/src/main/java/com/jary/progress/juc/executor/AsyncExecutor.java
@@ -0,0 +1,51 @@
+package com.jary.progress.juc.executor;
+
+
+import com.google.common.util.concurrent.ThreadFactoryBuilder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.concurrent.*;
+
+/**
+ * @author fanzhengjie
+ * @date 2020/5/17 7:58 下午
+ */
+public class AsyncExecutor {
+
+    private static final Logger log = LoggerFactory.getLogger(AsyncExecutor.class);
+
+    /**
+     * 当前cpu核心数
+     */
+    private static final int CORE_SIZE = Runtime.getRuntime().availableProcessors();
+
+    /**
+     * 拒绝策略：如果添加到线程池失败，主线程会自己去执行该任务
+     */
+    private static RejectedExecutionHandler REJECT = new ThreadPoolExecutor.CallerRunsPolicy();
+
+    /**
+     * 采用LinkedBlockingQueue作为等待队列，生产和消费的锁是分离的，比较适合请求和执行频率差不多的场景
+     */
+    private static LinkedBlockingQueue<Runnable> QUEUE = new LinkedBlockingQueue<>();
+
+    private static ThreadFactory THREAD_FACTORY = new ThreadFactoryBuilder()
+            .setNameFormat("progress-executor-%s")
+            .setUncaughtExceptionHandler((t,e) -> log.error("progress线程池未知异常, threadName:{}", t.getName(), e))
+            .build();
+
+    /**
+     * 假设一个请求平均执行时间为10ms，则单线程一秒钟能执行100个请求（有IO等待或者强制阻塞的不考虑）
+     * 此线程池被用于pay-service所有场景，假设异步的场景最高并发量为400，则峰值的时候最少需要4线程
+     * cpu资源足够的情况，维持cpu核心数的核心线程数，cpu+max并发量的最大线程数
+     */
+    private static ExecutorService EXECUTOR_SERVICE = new ThreadPoolExecutor(CORE_SIZE, CORE_SIZE + 4, 30, TimeUnit.SECONDS, QUEUE, null
+            , REJECT);
+
+    public static ExecutorService getExecutorService(){
+        return EXECUTOR_SERVICE;
+    }
+}
+
+
