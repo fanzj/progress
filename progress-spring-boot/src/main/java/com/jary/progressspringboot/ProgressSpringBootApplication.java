f2c2f888d56b9cfda93559f77d1d0bb8ddec107c
fanzhengjie <fanzhengjie@youzan.com>
2020-05-28 11:48:46 +0800

    添加spring-boot模块，该模块单独

---
+++ progress-spring-boot/src/main/java/com/jary/progressspringboot/ProgressSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.jary.progressspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgressSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProgressSpringBootApplication.class, args);
+    }
+
+}
---
+++ progress-spring-boot/src/test/java/com/jary/progressspringboot/ProgressSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jary.progressspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProgressSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
