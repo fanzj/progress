e468495bdfbcbb845f6cd9c003cdfc49be80107d
fanzhengjie <fanzhengjie@youzan.com>
2020-06-03 15:50:07 +0800

    guava collection & csv

---
+++ progress-common/src/main/java/com/jary/progress/common/guava/collection/ImmutableSetDemo.java
@@ -0,0 +1,24 @@
+package com.jary.progress.common.guava.collection;
+
+import com.google.common.collect.ImmutableSet;
+
+/**
+ * @author fanzhengjie
+ * @create 2018/9/19 下午8:28
+ * @description 不可变集合
+ */
+public class ImmutableSetDemo {
+
+    public static final ImmutableSet<String> COLOR_NAMES = ImmutableSet.of(
+        "red",
+        "orange",
+        "yellow",
+        "green",
+        "blue",
+        "purple");
+
+    public static void main(String[] args) {
+
+    }
+
+}
